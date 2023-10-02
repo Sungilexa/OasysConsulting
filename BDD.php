d63d9561a57297a7924e37f541011f7503c5890a
Noobizze <noobizzeyt@gmail.com>
2023-10-02 12:10:01 +0200

    Commit 02/10

---
+++ BDD.php
@@ -0,0 +1,14 @@
+<?php
+$host = '192.168.244.148';
+$dbname = 'Oasys consulting';
+$username = 'admin';
+$password = 'admin';
+
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}   catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+}
+
+?>
